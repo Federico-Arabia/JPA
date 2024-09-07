0cabe9ab9d264c07bf2366abd795822df7ce91e6
Federico-Arabia <tetearabia@gmail.com>
2024-09-07 13:56:34 -0300

    Add files via upload

---
+++ jpa-main/src/main/java/Entidades/Categoria.java
@@ -0,0 +1,27 @@
+package Entidades;
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "Categoria")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class Categoria implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String denominacion;
+
+    @ManyToMany(mappedBy = "categorias")
+    @Builder.Default
+    private Set<Articulo> articulos = new HashSet<>();
+}
